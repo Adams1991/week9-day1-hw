b2aeaf5250ce34bb9fa9da05bfde43c15dc20829
Adams1991 <adams121@hotmail.co.uk>
2018-07-16 19:08:54 +0100

    /one restful route working

---
+++ src/main/java/NameListController.java
@@ -0,0 +1,34 @@
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static spark.Spark.get;
+import static spark.Spark.staticFileLocation;
+
+public class NameListController {
+    public static void main(String[] args) {
+        VelocityTemplateEngine velocityTemplateEngine = new VelocityTemplateEngine();
+//        staticFileLocation("/public");
+
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Bill");
+        names.add("Bob");
+        names.add("Hannah");
+        names.add("Luna");
+        NameList nameList = new NameList(names);
+
+
+
+        get("/one", (req, res) -> {
+
+            HashMap<String, Object> model = new HashMap<>();
+            model.put("name", nameList.getRandomName());
+            return new ModelAndView(model, "onename.vtl");
+        }, velocityTemplateEngine);
+
+
+
+    }
+}
