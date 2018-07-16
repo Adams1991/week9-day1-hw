59ecc14826935ccdba5bbd6aee4a015034537b0b
Adams1991 <adams121@hotmail.co.uk>
2018-07-16 18:38:48 +0100

    basic setter and getter passing test

--- src/test/java/NameListTest.java
+++ src/test/java/NameListTest.java
@@ -3,23 +3,32 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 
+import static org.junit.Assert.assertEquals;
+
 public class NameListTest {
+    private NameList nameList;
+    ArrayList<String> nameArray;
+    ArrayList<String> nameArray2;
 
     @Before
-    public void setUp() throws Exception {
-        ArrayList<String> nameArray = new ArrayList<>();
+    public void setUp(){
+        nameArray = new ArrayList<>();
+        nameArray2 = new ArrayList<>();
         nameArray.add("Bill");
         nameArray.add("Bob");
         nameArray.add("Hannah");
         nameArray.add("Luna");
-        NameList nameList = new NameList(nameArray);
+        nameList = new NameList(nameArray);
     }
 
     @Test
     public void getNames() {
+        assertEquals(nameArray, nameList.getNames());
     }
 
     @Test
     public void setNames() {
+        nameList.setNames(nameArray2);
+        assertEquals(nameArray2, nameList.getNames());
     }
 }
